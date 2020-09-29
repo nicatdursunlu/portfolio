3837601109a3096fa9fc320fc5062497e1b2a868
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-09-29 14:12:22 +0400

    scroll added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,46 +26,48 @@ $(document).ready(function () {
   // typing aniation script
   const typed = new Typed(".typing", {
     strings: ["Front-End Developer", "Mobile Developer", "Freelancer"],
-    typeSpeed: 80,
-    backSpeed: 50,
+    typeSpeed: 100,
+    backSpeed: 80,
     loop: true,
   });
 
   const typed2 = new Typed(".typing-2", {
     strings: ["Front-End Developer", "Mobile Developer", "Freelancer"],
-    typeSpeed: 80,
-    backSpeed: 50,
+    typeSpeed: 100,
+    backSpeed: 80,
     loop: true,
   });
 
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+  });
 
-  // const sr = ScrollReveal({
-  //   origin: "top",
-  //   distance: "80px",
-  //   duration: 2000,
-  //   reset: true,
-  // });
-  
-  // sr.reveal(".text-1", {delay: 200});
-  // sr.reveal(".hire-me", { delay: 200 });
-  // sr.reveal(".home__img", { delay: 400 });
-  // sr.reveal(".home__social-icon", { interval: 200 });
-  
-  // /*SCROLL ABOUT*/
-  // sr.reveal(".about__img", {});
-  // sr.reveal(".about__subtitle", { delay: 400 });
-  // sr.reveal(".about__text", { delay: 400 });
-  
-  // /*SCROLL SKILLS*/
-  // sr.reveal(".skills__subtitle", {});
-  // sr.reveal(".skills__text", {});
-  // sr.reveal(".skills__data", { interval: 200 });
-  // sr.reveal(".skills__img", { delay: 600 });
-  
-  // /*SCROLL WORK*/
-  // sr.reveal(".work__img", { interval: 200 });
-  
-  // /*SCROLL CONTACT*/
-  // sr.reveal(".contact__input", { interval: 200 });
+  /* SCROLL COMMON */
+  sr.reveal(".services-content", {});
+  sr.reveal(".education-content", {});
+  sr.reveal(".education-content", {});
+  sr.reveal(".works-container", {});
+  sr.reveal(".left", { delay: 300 });
+  sr.reveal(".right", { delay: 900 });
+
+  /* SCROLL HOME */ 
+  sr.reveal(".text-1", { delay: 200 });
+  sr.reveal(".hire-me", { delay: 200 });
+  sr.reveal(".social-media", { interval: 200 });
+  sr.reveal(".home__img", { delay: 400 });
+
+  /*SCROLL SKILLS*/
+  sr.reveal(".skills__subtitle", {});
+  sr.reveal(".skills__text", {});
+  sr.reveal(".skills__data", { interval: 200 });
+  sr.reveal(".skills__img", { delay: 600 });
+
+  /*SCROLL WORK*/
+  sr.reveal(".work__img", { interval: 200 });
+
+  /*SCROLL CONTACT*/
+  sr.reveal(".contact__input", { interval: 200 });
 });
-
