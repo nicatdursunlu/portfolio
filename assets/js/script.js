263df5d038e03d3076bad2204b3ad49f0365692d
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-09-29 12:07:27 +0400

    works section

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,4 +22,50 @@ $(document).ready(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
+
+  // typing aniation script
+  const typed = new Typed(".typing", {
+    strings: ["Front-End Developer", "Mobile Developer", "Freelancer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true,
+  });
+
+  const typed2 = new Typed(".typing-2", {
+    strings: ["Front-End Developer", "Mobile Developer", "Freelancer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true,
+  });
+
+
+  // const sr = ScrollReveal({
+  //   origin: "top",
+  //   distance: "80px",
+  //   duration: 2000,
+  //   reset: true,
+  // });
+  
+  // sr.reveal(".text-1", {delay: 200});
+  // sr.reveal(".hire-me", { delay: 200 });
+  // sr.reveal(".home__img", { delay: 400 });
+  // sr.reveal(".home__social-icon", { interval: 200 });
+  
+  // /*SCROLL ABOUT*/
+  // sr.reveal(".about__img", {});
+  // sr.reveal(".about__subtitle", { delay: 400 });
+  // sr.reveal(".about__text", { delay: 400 });
+  
+  // /*SCROLL SKILLS*/
+  // sr.reveal(".skills__subtitle", {});
+  // sr.reveal(".skills__text", {});
+  // sr.reveal(".skills__data", { interval: 200 });
+  // sr.reveal(".skills__img", { delay: 600 });
+  
+  // /*SCROLL WORK*/
+  // sr.reveal(".work__img", { interval: 200 });
+  
+  // /*SCROLL CONTACT*/
+  // sr.reveal(".contact__input", { interval: 200 });
 });
+
